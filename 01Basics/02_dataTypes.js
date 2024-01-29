8327a17ea905b1edb01c83f2b4fb251820bb81de
Pranav Kumar <77223852+syThe0P@users.noreply.github.com>
2024-01-29 13:43:10 +0000

    Data Types

---
+++ 01Basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"Use Strict" // Treat all js code as a newer version
+
+// alert(3+3)  //This will not run because it is node not a browser
+
+console.log(3+3);
+                             //Code redability should be high
+console.log("Pranav");
+
+//Data types
+
+let name = "Pranav";
+let age = 19;
+let char = 'a';
+let isLoggedIn = true;
+let city; //this is undefined;
+
+/*
+number - range 2 - 53
+bigint - for higher range
+bool = true/false;
+string
+null - this is a null value that is assigned to a variable, it is a standalone value
+undefined - this is when the value to the variable is not assigned
+symbol  - unique values
+*/
+
+
+//objects
+
+console.log(typeof "Pranav"); //this will also tell the data type
+console.log(typeof age);  //this will tell the data type of particular variable assigned
+
+console.log(typeof undefined); //the type of undefined is undefined only;
+console.log(typeof null); //This will give the output of an object;
