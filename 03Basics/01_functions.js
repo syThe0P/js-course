ec603d816652b7b195733a16a3406b4369bb3932
Pranav Kumar <77223852+syThe0P@users.noreply.github.com>
2024-02-08 08:18:10 +0000

    few more changes

--- 03Basics/01_functions.js
+++ 03Basics/01_functions.js
@@ -25,10 +25,10 @@ function addNumbers(number1, number2){
     const result = number1 + number2;
     return result;
 }
-console.log("result :", addNumbers(2,4));
+// console.log("result :", addNumbers(2,4));
 
 const value = addNumbers(2, 4);
-console.log(value);
+// console.log(value);
 
 
 
@@ -37,4 +37,36 @@ function eccomerce(username){
 
 }
 
-console.log(eccomerce("Pranav kumar"))
+// console.log(eccomerce("Pranav kumar"))
+
+//Suppose we are making a function and we dont know how many argumnts will come so we use rest operator for this
+
+function calculateCartPrice(...num1){
+    return num1;
+}
+
+// console.log(calculateCartPrice(200,300,400, 500,600))  //so this rest operator will pass all the values inside of an array
+
+//Handle object with functions
+
+const user = {
+    name: "Pranav",
+    price: 199
+}
+
+function newFunc(anyobject){
+    return `username is ${anyobject.name}, and price is ${anyobject.price}`;
+}
+
+console.log(newFunc(user));
+
+
+//Handle array with functions
+
+const array = [100, 200, 300, 400];
+
+function secFunc(anyarray){
+    return `The second value is ${anyarray[1]}`;
+}
+
+console.log(secFunc(array));
